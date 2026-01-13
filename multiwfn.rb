8521dd330359a544f6f1bc47dcb931a5edb14c4a
Kjell Jorner <kjell.jorner@gmail.com>
2026-01-13 21:20:58 +0100

    Refactor Multiwfn class descriptions and dependencies

--- multiwfn.rb
+++ multiwfn.rb
@@ -1,25 +1,23 @@
 class Multiwfn < Formula
-  desc "A Multifunctional Wavefunction Analyzer"
+  desc "Multifunctional Wavefunction Analyzer"
+  desc "A copy of settings.ini is installed."
+  desc "To use it, set environment variable Multiwfnpath=${HOMEBREW_PREFIX}/etc/multiwfn."
   homepage "http://sobereva.com/multiwfn/"
   head "https://github.com/digital-chemistry-laboratory/multiwfn-mac-build.git", branch: "source_dist"
   license :cannot_represent
-
-  desc "A copy of settings.ini is installed. To use it, set environment variable Multiwfnpath=${HOMEBREW_PREFIX}/etc/multiwfn."
 
   option "with-openmp", "Builds with OpenMP support."
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gcc"
-  depends_on "openblas"
   depends_on "flint"
+  depends_on "openblas"
   fails_with :clang
 
   def install
     cmake_args = std_cmake_args
-    if build.with? "openmp"
-      cmake_args << "-DWITH_OpenMP=ON"
-    end
+    cmake_args << "-DWITH_OpenMP=ON" if build.with? "openmp"
     system "cmake", "-S", ".", "-B", "build", *cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
