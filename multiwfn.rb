8f1d92f40a7737991168817d88ec78660eb35336
Kjell Jorner <kjell.jorner@gmail.com>
2026-01-14 11:21:36 +0100

    Update formula and GitHub actions workflow
    * Update Homebrew install command for multiwfn
    
    * Refactor Multiwfn class descriptions and dependencies

--- multiwfn.rb
+++ multiwfn.rb
@@ -1,31 +1,29 @@
 class Multiwfn < Formula
-  desc "A Multifunctional Wavefunction Analyzer"
+  desc "Multifunctional Wavefunction Analyzer"
+  desc "A copy of settings.ini is installed."
+  desc "To use it, set environment variable Multiwfnpath=${HOMEBREW_PREFIX}/etc/multiwfn."
   homepage "http://sobereva.com/multiwfn/"
-  head "https://github.com/digital-chemistry-laboratory/multiwfn-mac-build.git", branch: "source_dist"
   license :cannot_represent
-
-  desc "A copy of settings.ini is installed. To use it, set environment variable Multiwfnpath=${HOMEBREW_PREFIX}/etc/multiwfn."
+  head "https://github.com/digital-chemistry-laboratory/multiwfn-mac-build.git", branch: "source_dist"
 
   option "with-openmp", "Builds with OpenMP support."
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "flint"
   depends_on "gcc"
   depends_on "openblas"
-  depends_on "flint"
   fails_with :clang
 
   def install
     cmake_args = std_cmake_args
-    if build.with? "openmp"
-      cmake_args << "-DWITH_OpenMP=ON"
-    end
+    cmake_args << "-DWITH_OpenMP=ON" if build.with? "openmp"
     system "cmake", "-S", ".", "-B", "build", *cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    system "expect", "-c", "'spawn multiwfn; send \\003;'"
+    system "expect", "-c", "spawn multiwfn; send \\003;"
   end
 end
